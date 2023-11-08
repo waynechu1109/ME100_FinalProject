0935f3bdc4265012cc74f1daf33f90ef5a35947f
Wayne Chu <99809105+waynechu1109@users.noreply.github.com>
2023-11-08 15:59:58 -0800

    20231108_1559
    detect function

--- ultrasonic_test.py
+++ ultrasonic_test.py
@@ -3,15 +3,31 @@ import time
 import math
 
 # Pin numbers for ESP32 (adjust as needed)
-GPIO_TRIGGER = 26  # GPIO5 for trigger
-GPIO_ECHO = 25     # GPIO4 for echo
+sensor_1_trigger = 26  # GPIO5 for trigger
+sensor_1_echo = 25     # GPIO4 for echo
+
+sensor_2_trigger = 27
+sensor_2_echo = 33
+
 LED = 12
 
-trigger = Pin(GPIO_TRIGGER, Pin.OUT)
-echo = Pin(GPIO_ECHO, Pin.IN)
+dist_arr_1 = [0, 0]
+period = 0.1
+i = 0
+activate = False
+
+# trigger = Pin(sensor_1_trigger, Pin.OUT)
+# echo = Pin(sensor_1_echo, Pin.IN)
 led = Pin(LED, mode=Pin.OUT)
 
-def distance():
+def distance(label):
+    if label == 1:
+        trigger = Pin(sensor_1_trigger, Pin.OUT)
+        echo = Pin(sensor_1_echo, Pin.IN)
+    elif label == 2:
+        trigger = Pin(sensor_2_trigger, Pin.OUT)
+        echo = Pin(sensor_2_echo, Pin.IN)
+        
     trigger.value(1)
     time.sleep_us(10)
     trigger.value(0)
@@ -34,36 +50,39 @@ def blinking_led():
         time.sleep(led_period)
         led.value(0)
         time.sleep(led_period)
+        
+def detect(label):
+    global dist_arr_1, period, i, activate
 
-dist_arr = [0, 0]
-period = 0.1
-i = 0
-activate = False
+    dist_1 = distance(label)
+#   print("Measured Distance = %.1f cm" % dist)
+    dist_arr_1[1] = dist_arr_1[0]
+    dist_arr_1[0] = dist_1
+    if (dist_arr_1[0] is not 0) and (dist_arr_1[1] is not 0):
+        velo = (dist_arr_1[0]-dist_arr_1[1])/period
+        if math.fabs(velo) >= 5: # the absolute value of the velo
+            i += 1
+        else:
+            i = 0
+        
+        if i == 10:
+            activate = True
+            return
+        
+        print("Measured Velocity = %.1f cm/s" % velo)
+    time.sleep(period)
+    
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = distance()
-#             print("Measured Distance = %.1f cm" % dist)
-            dist_arr[1] = dist_arr[0]
-            dist_arr[0] = dist
-            if (dist_arr[0] is not 0) and (dist_arr[1] is not 0):
-                velo = (dist_arr[0]-dist_arr[1])/period
-                if math.fabs(velo) >= 5: # the absolute value of the velo
-                    i += 1
-                else:
-                    i = 0
-                
-                if i == 10:
-                    activate = True
-                    break
-                
-                print("Measured Velocity = %.1f cm/s" % velo)
-            time.sleep(period)
+            detect(1)
         
-        if activate:
+            if activate:
                 print('ACTIVATE!')
                 blinking_led()
+                activate = False
+                i = 0
  
     except KeyboardInterrupt:
         print("Measurement stopped by User")

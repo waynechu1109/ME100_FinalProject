866c9a4e63ccdf92d2a9e1148b06aa29a8ec535a
Wayne Chu <99809105+waynechu1109@users.noreply.github.com>
2023-11-12 15:52:14 -0800

    20231112_1552
    buzzling

--- ultrasonic_test.py
+++ ultrasonic_test.py
@@ -1,6 +1,7 @@
 from umqtt.simple import MQTTClient
-from machine import Pin, time_pulse_us
+from machine import Pin, time_pulse_us, PWM, Timer
 import time
+from time import sleep
 import math
 import uasyncio as asyncio
 import network
@@ -37,6 +38,80 @@ sensor_2_echo = 33
 
 LED = 12
 
+buz = Pin(15, mode=Pin.OUT)
+duty_cycle = 100
+L1 = PWM(buz,freq=500,duty=duty_cycle)
+
+# various notes and pitches
+C3 = 131
+CS3 = 139
+D3 = 147
+DS3 = 156
+E3 = 165
+F3 = 175
+FS3 = 185
+G3 = 196
+GS3 = 208
+A3 = 220
+AS3 = 233
+B3 = 247
+C4 = 262
+CS4 = 277
+D4 = 294
+DS4 = 311
+E4 = 330
+F4 = 349
+FS4 = 370
+G4 = 392
+GS4 = 415
+A4 = 440
+AS4 = 466
+B4 = 494
+C5 = 523
+CS5 = 554
+D5 = 587
+DS5 = 622
+E5 = 659
+F5 = 698
+FS5 = 740
+G5 = 784
+GS5 = 831
+A5 = 880
+AS5 = 932
+B5 = 988
+C6 = 1047
+CS6 = 1109
+D6 = 1175
+DS6 = 1245
+E6 = 1319
+F6 = 1397
+FS6 = 1480
+G6 = 1568
+GS6 = 1661
+A6 = 1760
+AS6 = 1865
+B6 = 1976
+C7 = 2093
+CS7 = 2217
+D7 = 2349
+DS7 = 2489
+E7 = 2637
+F7 = 2794
+FS7 = 2960
+G7 = 3136
+GS7 = 3322
+A7 = 3520
+AS7 = 3729
+B7 = 3951
+C8 = 4186
+CS8 = 4435
+D8 = 4699
+DS8 = 4978
+
+music = [
+G5
+]
+
 dist_arr = [[0, 0], [0, 0]]
 period = 0.1
 i = [0, 0]
@@ -45,6 +120,18 @@ activate = False
 # trigger = Pin(sensor_1_trigger, Pin.OUT)
 # echo = Pin(sensor_1_echo, Pin.IN)
 led = Pin(LED, mode=Pin.OUT)
+
+note_index = 0
+# This is the callback function
+def tcb(timer):
+    global duty_cycle
+    global note_index
+    if note_index < len(music)-1:
+        note_index += 1
+    else:
+        note_index = 0
+    
+    L1.freq(music[note_index])
 
 def distance(label):
     if label == 1:
@@ -71,9 +158,18 @@ def distance(label):
 
 def blinking_led():
     led_period = 0.5
+    buz_period = led_period*1000
     for _ in range(5):
         led.value(1)
         time.sleep(led_period)
+        
+        t1 = Timer(1)
+        # Callback means that everytime the timer
+        # counts one, the corresponding function (in this case tcb)
+        # will be executed.
+        t1.init(period=500, mode=t1.PERIODIC, callback=tcb)
+        
+        
         led.value(0)
         time.sleep(led_period)
         
@@ -97,6 +193,13 @@ def detect(label):
         
         print("Label:", label, "Measured Velocity = %.1f cm/s" % velo)
     time.sleep(period)
+    
+# def buzzle():
+#     t1 = Timer(1)
+#     # Callback means that everytime the timer
+#     # counts one, the corresponding function (in this case tcb)
+#     # will be executed.
+#     t1.init(period=250, mode=t1.PERIODIC, callback=tcb)
     
 
 if __name__ == '__main__':
